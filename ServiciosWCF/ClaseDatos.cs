6624f042bd7774f08902947f754fbb7903ca902e
ccallado <ccallado2005@gmail.com>
2013-07-22 20:50:08 +0200

    Otra mas

--- ServiciosWCF/ClaseDatos.cs
+++ ServiciosWCF/ClaseDatos.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+//Para ignorar la serialización de algun parametro
+using System.Runtime.Serialization;
+
 namespace ServiciosWCF
 {
+    //Si ponemos esta label se serializará solo aquellos que lleven [DataMember]
+    [DataContract]
     public class ClaseDatos
     {
+        [DataMember]
+        //Aunque sea local tambien lo serializa
+        int IdLocal { get; set; }
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nombre { get; set; }
+
+        //Si quiero que alguna no quiero que se serialice
+        [IgnoreDataMember]
+        public DateTime Creacion { get; set; }
     }
 }
